5efaab349e05152bd231a0d577538e3231220bbe
omartuwaiq <91540868+omartuwaiq@users.noreply.github.com>
2022-01-21 15:35:22 +0300

    Delete src directory

--- src/main/java/com/example/Recruiting_Office/model/Repository/ContractorRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.Recruiting_Office.model.Repository;
-
-import com.example.Recruiting_Office.model.Entities.Contractor;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContractorRepository extends JpaRepository<Contractor,Integer> {
-}
--- src/main/java/com/example/Recruiting_Office/model/Repository/ServicesRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Recruiting_Office.model.Repository;
-
-import com.example.Recruiting_Office.model.Entities.Services;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ServicesRepository extends JpaRepository<Services,Integer> {
-}
