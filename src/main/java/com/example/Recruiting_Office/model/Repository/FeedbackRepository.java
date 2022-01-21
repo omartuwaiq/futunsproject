5ccbf216736ca9f0f66434427a6b77939c17fa50
omartuwaiq <91540868+omartuwaiq@users.noreply.github.com>
2022-01-21 15:44:33 +0300

    Delete src directory

--- src/main/java/com/example/Recruiting_Office/model/Repository/FeedbackRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Recruiting_Office.model.Repository;
-
-import com.example.Recruiting_Office.model.Entities.Feedback;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FeedbackRepository  extends JpaRepository<Feedback,Integer> {
-}
