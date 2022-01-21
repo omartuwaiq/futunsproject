4bf02aa20627f4faadf50deba4bf9714544a9f6d
omartuwaiq <91540868+omartuwaiq@users.noreply.github.com>
2022-01-21 16:03:50 +0300

    Add files via upload

---
+++ src/main/java/com/example/Recruiting_Office/model/Repository/ServicesRepository.java
@@ -0,0 +1,7 @@
+package com.example.Recruiting_Office.model.Repository;
+
+import com.example.Recruiting_Office.model.Entities.Services;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServicesRepository extends JpaRepository<Services,Integer> {
+}
